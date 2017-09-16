6fac38c45b2a24eeb58c02a87c223327d0daf3ee
Rafah-Mehfooz <mmehfooz.bese15seecs@seecs.edu.pk>
2017-09-16 18:41:32 +0500

    create account with error

---
+++ character.daily/js/custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $("#passcode").mask("999-9999-999");
+});
+
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
+$(document).ready(function () {
+    $('[data-toggle="popover"]').popover()
+})
