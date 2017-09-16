844a8d36955c8940618ab6f066cb62b181c87e92
Rafah-Mehfooz <mmehfooz.bese15seecs@seecs.edu.pk>
2017-09-16 16:02:56 +0500

    changes

---
+++ Dark-theme-landing-page/subscribe.php
@@ -0,0 +1,16 @@
+<?php	
+	require_once '';
+	$api = new MCAPI('');	
+	$merge_vars = array('FNAME'=>$_POST["fname"], 'LNAME'=>$_POST["lname"], 'WEBS'=>$_POST["wname"]);
+	
+	// Submit subscriber data to MailChimp
+	// For parameters doc, refer to: http://apidocs.mailchimp.com/api/1.3/listsubscribe.func.php
+	$retval = $api->listSubscribe( '', $_POST["email"], $merge_vars, 'html', false, true );
+	
+	if ($api->errorCode){
+		echo '<h4 style="padding: 5px; color: red;">Please try again.</h4>';
+	} else {
+		echo '<h4 style="padding: 5px;">Thanks for joining the Olanola Blogger Network</h4>';
+	}
+?>
+
---
+++ index.md
@@ -0,0 +1,2 @@
+## Welcome to my Demopages
+![Alt text](/relative/path/to/img.jpg?raw=true "Optional Title")
