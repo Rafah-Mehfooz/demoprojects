397be5e890a277dbad14bde946562fa23fb95671
Rafah-Mehfooz <mmehfooz.bese15seecs@seecs.edu.pk>
2017-11-23 21:42:02 +0500

    shostyle-collective items added

--- Light-theme-Landing-Page/subscribe.php
+++ Light-theme-Landing-Page/subscribe.php
@@ -1,25 +1,16 @@
 <?php	
-<<<<<<< HEAD
 	require_once '';
 	$api = new MCAPI('');	
-=======
-	require_once 'inc/MCAPI.class.php';
-	$api = new MCAPI('39cf295dd0257c39de691c8518d96efe-us8');	
->>>>>>> f8e299c576bd34def775c4df9922a52494af5db2
 	$merge_vars = array('FNAME'=>$_POST["fname"], 'LNAME'=>$_POST["lname"], 'WEBS'=>$_POST["wname"]);
 	
 	// Submit subscriber data to MailChimp
 	// For parameters doc, refer to: http://apidocs.mailchimp.com/api/1.3/listsubscribe.func.php
-<<<<<<< HEAD
 	$retval = $api->listSubscribe( '', $_POST["email"], $merge_vars, 'html', false, true );
-=======
-	$retval = $api->listSubscribe( '44a95d2467', $_POST["email"], $merge_vars, 'html', false, true );
->>>>>>> f8e299c576bd34def775c4df9922a52494af5db2
 	
 	if ($api->errorCode){
 		echo '<h4 style="padding: 5px; color: red;">Please try again.</h4>';
 	} else {
-		echo '<h4 style="padding: 5px; color: black;">Thanks for joining the Olanola Blogger Network</h4>';
+		echo '<h4 style="padding: 5px;">Thanks for joining the Olanola Blogger Network</h4>';
 	}
 ?>
 
